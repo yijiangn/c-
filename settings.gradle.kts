38094ad79f1fc33b6cb41cc071b9b29a5f8ab095
Agus Pratama Hartono <168091936+yijiangn@users.noreply.github.com>
2026-08-28 18:28:33 +0800

    chore: add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "学习记录"
+include(":app")
